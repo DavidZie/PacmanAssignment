a7aa91c9d30e7eaf263af2db087cca7cdd837fac
pelegavraham <pelegav@post.bgu.ac.il>
2018-05-30 11:29:32 +0300

    add song

--- src/GameComponents/MainFrame.java
+++ src/GameComponents/MainFrame.java
@@ -1,4 +1,9 @@
 package GameComponents;
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -7,6 +12,7 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import static Logic.Globals.imagesPath;
@@ -16,6 +22,7 @@ public class MainFrame extends JFrame {
     JFrame mainFrame;
     JPanel containerPanel;
     GameFrame gameFrame;
+    public static final String songPath = System.getProperty("user.dir");
 
     public MainFrame(){
         mainFrame = new JFrame("Pac-Man");//Create Frame
@@ -23,6 +30,7 @@ public class MainFrame extends JFrame {
         mainFrame.setSize(840,590);//Set Frame dimensions to 400 width and 400 height
         mainFrame.setLocationRelativeTo(null);//Center the frame on the screen.
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//Make the program Abort When user closes the Frame.
+        music();
         try {
             containerPanel=new JPanelWithBackground(imagesPath+"\\background.png");
             JButton startGameButton = new JButton("Start Game!");//Create Button with text.
@@ -54,7 +62,18 @@ public class MainFrame extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }//Create Main Frame with Buttons and assign actions to buttons.
 
+        private void music(){
+            AudioPlayer MPG=AudioPlayer.player;
+            AudioStream BGM;
+            AudioData MD;
+            ContinuousAudioDataStream loop=null;
+            try {
+                BGM=new AudioStream(new FileInputStream(songPath+"\\PacmanRemix.wav"));
+                MD=BGM.getData();
+                loop=new ContinuousAudioDataStream(MD);
+            }catch (IOException error){}
+            MPG.start(loop);
+        }
 }
