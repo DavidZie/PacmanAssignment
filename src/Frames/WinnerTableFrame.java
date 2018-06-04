fdd694435b3730028a96858efe578029907e0f5a
pelegavraham <pelegav@post.bgu.ac.il>
2018-06-05 00:06:58 +0300

    set Table winner args

--- src/Frames/WinnerTableFrame.java
+++ src/Frames/WinnerTableFrame.java
@@ -76,12 +76,57 @@ public class WinnerTableFrame {
     }
 
     private void addRowToTable(String name, int[] points){
-        int index=0;
-        while (data[index][0]!="")
-            index++;
-        data[index][0]=name;
-        for (int i=0;i<points.length;i++)
-            data[index][i+1]=points[i];
+        //Object[] curr=new Object[7];
+        //curr[0] = name;
+        //for (int i = 1; i < curr.length; i++)
+        //    curr[i] = points[i-1];
+        //for (int i=0;i<curr.length;i++)
+         //   data[4][i]=curr[i];
+        //int index=4;
+        //while (index>=0) {
+        //    index--;
+        //    swapRows(index,data,name,curr);
+        //}
+        for (int i=0;i<data[4].length-1;i++)
+            data[4][i+1]=points[i];
+        data[4][0]=name;
+
+        swapInts(data, points);
+        swapName(data, name);
+    }
+
+    private void swapInts(Object[][] data, int[] points) {
+        int[] tmp=points;
+        int index=4;
+        while (index>0){
+            for (int i=0;i<data[index].length-1;i++)
+                data[index][i+1]=data[index-1][i+1];
+            index--;
+        }
+        for (int i=0;i<data[0].length-1;i++)
+            data[0][i+1]=points[i];
+    }
+
+    private void swapName(Object[][] data, String name) {
+        String tmp=name;
+        int index=4;
+        while (index>0){
+            data[index][0]=data[index-1][0];
+            index--;
+        }
+        data[0][0]=tmp;
+    }
+
+    private void swapRows(int index, Object[][] data,String name, Object[] curr) {
+
+        Object[] tmp=curr;
+
+        for (int i = 0; i < data[index].length; i++)
+        data[index+1][i]=data[index][i];
+
+        for (int i = 0; i < data[index].length; i++)
+        data[index][i]=tmp[i];
+
     }
 
     private void addButton() {
